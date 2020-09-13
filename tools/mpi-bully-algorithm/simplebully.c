b9487ab8f1b4b8df592ae2441891c5801318239c
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-09-13 14:38:47 +0530

    code cleanup

--- tools/mpi-bully-algorithm/simplebully.c
+++ tools/mpi-bully-algorithm/simplebully.c
@@ -4,28 +4,30 @@
 int MAX_ROUNDS = 6;
 double TX_PROB = 1.0 - ERROR_PROB;
 
-unsigned long int get_PRNG_seed() {
+unsigned long int get_PRNG_seed()
+{
 	struct timeval tv;
-	gettimeofday(&tv,NULL);
+	gettimeofday(&tv, NULL);
 	unsigned long time_in_micros = 1000000 * tv.tv_sec + tv.tv_usec + getpid();
 
 	return time_in_micros;
 }
 
-bool try_leader_elect() {
+bool try_leader_elect()
+{
 
-	double prob = rand() / (double) RAND_MAX;
+	double prob = rand() / (double)RAND_MAX;
 	bool leader_elect = (prob > THRESHOLD);
 
 	return leader_elect;
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
 	int myrank, np;
 	int current_leader = 0;
-	int *recv_buf = (int *) malloc(2 * sizeof(int));
+	int *recv_buf = (int *)malloc(2 * sizeof(int));
 	int hello = HELLO_MSG;
 
 	// user input argv[1]: designated initial leader
@@ -41,12 +43,12 @@ int main(int argc, char *argv[]) {
 	printf("\n*******************************************************************");
 	printf("\n*******************************************************************\n\n");
 
-  // MPI Initiliazation
+	// MPI Initiliazation
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(comm, &myrank);
 	MPI_Comm_size(comm, &np);
 
-  MPI_Status status;
+	MPI_Status status;
 
 	srand(get_PRNG_seed());
 
@@ -60,11 +62,11 @@ int main(int argc, char *argv[]) {
 	// But if we are not rank 0, then our pred is just one below us, myrank - 1
 	if (myrank == 0)
 	{
-			pred = np - 1;
+		pred = np - 1;
 	}
 	else
 	{
-			pred = myrank - 1;
+		pred = myrank - 1;
 	}
 
 	// Again, since the nodes are in a ring, if we are the highest rank, we loop around
@@ -72,27 +74,30 @@ int main(int argc, char *argv[]) {
 	// But if we are not the highest rank, our succ is just one above us myrank + 1
 	if (myrank == (np - 1))
 	{
-			succ = 0;
+		succ = 0;
 	}
 	else
 	{
-			succ = myrank + 1;
+		succ = myrank + 1;
 	}
 
 	// Main loop, each iteration is a round
-	for (int round = 0; round < MAX_ROUNDS; round++) {
+	for (int round = 0; round < MAX_ROUNDS; round++)
+	{
 		printf("\n*********************************** ROUND %d ******************************************\n", round);
 
 		// Check if we are the current leader
-		if (myrank == current_leader) {
-					// If we are current leader, then we can do two things:
-					// Send a hello message to succ or transmit leader_election_message to succ
-        	if (try_leader_elect()) {
-					// Our method to figuring out which to do is try_leader_elect
-					// If we decide to try leader elect:
-					// 1. Generate random TOKEN
-					// 2. Transmit leader_election_msg to succ
-					// 2.1 leader_election_msg contains rank of token and token value
+		if (myrank == current_leader)
+		{
+			// If we are current leader, then we can do two things:
+			// Send a hello message to succ or transmit leader_election_message to succ
+			if (try_leader_elect())
+			{
+				// Our method to figuring out which to do is try_leader_elect
+				// If we decide to try leader elect:
+				// 1. Generate random TOKEN
+				// 2. Transmit leader_election_msg to succ
+				// 2.1 leader_election_msg contains rank of token and token value
 				mytoken = generate_token();
 				recv_buf[0] = myrank;
 				recv_buf[1] = mytoken;
@@ -100,7 +105,9 @@ int main(int argc, char *argv[]) {
 				MPI_Send(recv_buf, 2, MPI_INT, succ, LEADER_ELECTION_MSG_TAG, comm);
 				printf("\n[rank %d][%d] SENT LEADER ELECTION MSG to node %d with TOKEN = %d, tag = %d\n", myrank, round, succ, mytoken, LEADER_ELECTION_MSG_TAG);
 				fflush(stdout);
-			} else {
+			}
+			else
+			{
 				MPI_Send(&hello, 1, MPI_INT, succ, HELLO_MSG_TAG, comm);
 				printf("\n[rank %d][%d] SENT HELLO MSG to node %d with TOKEN = %d, tag = %d\n", myrank, round, succ, mytoken, HELLO_MSG_TAG);
 				fflush(stdout);
@@ -114,69 +121,80 @@ int main(int argc, char *argv[]) {
 
 			while (!flag)
 			{
-					MPI_Test(&request, &flag, &status);
+				MPI_Test(&request, &flag, &status);
 			}
 
-			if (flag) {
+			if (flag)
+			{
 				// We got a message
-				switch (status.MPI_TAG) {
-					case HELLO_MSG_TAG:
-						// We sent the hello message and recieved from our pred, it hit all nodes
-						printf("\n[rank %d][%d] HELLO MESSAGE completed ring traversal!\n", myrank, round);
-						fflush(stdout);
-						break;
-					case LEADER_ELECTION_MSG_TAG:
-						// We got an election msg, change current_leader to msg[0] and send result to success
-						// Wait for result to come back from pred
-						current_leader = recv_buf[0];
-						MPI_Send(recv_buf,	2, MPI_INT, succ, LEADER_ELECTION_RESULT_MSG_TAG, comm);
-						MPI_Recv(recv_buf, 2, MPI_INT, pred, LEADER_ELECTION_RESULT_MSG_TAG, comm, MPI_STATUS_IGNORE);
-						printf("\n[rank %d][%d] NEW LEADER FOUND! new leader = %d, with token = %d\n", myrank, round, current_leader, recv_buf[1]);
-						fflush(stdout);
-						break;
-					default: ;
+				switch (status.MPI_TAG)
+				{
+				case HELLO_MSG_TAG:
+					// We sent the hello message and recieved from our pred, it hit all nodes
+					printf("\n[rank %d][%d] HELLO MESSAGE completed ring traversal!\n", myrank, round);
+					fflush(stdout);
+					break;
+				case LEADER_ELECTION_MSG_TAG:
+					// We got an election msg, change current_leader to msg[0] and send result to success
+					// Wait for result to come back from pred
+					current_leader = recv_buf[0];
+					MPI_Send(recv_buf, 2, MPI_INT, succ, LEADER_ELECTION_RESULT_MSG_TAG, comm);
+					MPI_Recv(recv_buf, 2, MPI_INT, pred, LEADER_ELECTION_RESULT_MSG_TAG, comm, MPI_STATUS_IGNORE);
+					printf("\n[rank %d][%d] NEW LEADER FOUND! new leader = %d, with token = %d\n", myrank, round, current_leader, recv_buf[1]);
+					fflush(stdout);
+					break;
+				default:;
 				}
 			}
-		} else {
+		}
+		else
+		{
 			// We are not the current leader
 			int flag = 0;
 
 			while (!flag)
 			{
-					MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &flag, &status);
+				MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &flag, &status);
 			}
 
-			if (flag) {
+			if (flag)
+			{
 
 				// Wait for message
 				MPI_Recv(recv_buf, 2, MPI_INT, pred, MPI_ANY_TAG, comm, &status);
 
-				if (status.MPI_TAG == HELLO_MSG_TAG) {
+				if (status.MPI_TAG == HELLO_MSG_TAG)
+				{
 					// Pass hello message to succ
 					MPI_Send(&hello, 1, MPI_INT, succ, HELLO_MSG_TAG, comm);
 					printf("\n\t[rank %d][%d] Received and Forwarded HELLO MSG to next node = %d\n", myrank, round, succ);
 					fflush(stdout);
-				} else if (status.MPI_TAG == LEADER_ELECTION_MSG_TAG) {
+				}
+				else if (status.MPI_TAG == LEADER_ELECTION_MSG_TAG)
+				{
 					// We got the leader_election msg
-					if (try_leader_elect()) {
+					if (try_leader_elect())
+					{
 						// Check if we will participate, same method as the leader uses
 						// If yes, generate token and check my values against current received
 						mytoken = generate_token();
 						if ((mytoken > recv_buf[1]) || ((mytoken == recv_buf[1]) && (myrank > recv_buf[0])))
 						{
-								recv_buf[0] = myrank;
-								recv_buf[1] = mytoken;
+							recv_buf[0] = myrank;
+							recv_buf[1] = mytoken;
 						}
 						printf("\n\t[rank %d][%d] My new TOKEN = %d\n", myrank, round, mytoken);
 						fflush(stdout);
-					} else {
+					}
+					else
+					{
 						// We are not going to participate, do nothing
-							printf("\n\t[rank %d][%d] Will not participate in Leader Election.\n", myrank, round);
-							fflush(stdout);
+						printf("\n\t[rank %d][%d] Will not participate in Leader Election.\n", myrank, round);
+						fflush(stdout);
 					}
 
 					// Regardless of if we participate or not, we send LEADER_ELECT_MSG to succ
-					MPI_Send(recv_buf, 2,	MPI_INT, succ, LEADER_ELECTION_MSG_TAG, comm);
+					MPI_Send(recv_buf, 2, MPI_INT, succ, LEADER_ELECTION_MSG_TAG, comm);
 
 					// Then issue a blocking MPI_RECV call and wait for results of election
 					MPI_Recv(recv_buf, 2, MPI_INT, MPI_ANY_SOURCE, LEADER_ELECTION_RESULT_MSG_TAG, comm, MPI_STATUS_IGNORE);
@@ -187,7 +205,6 @@ int main(int argc, char *argv[]) {
 					printf("\n\t[rank %d][%d] NEW LEADER :: node %d with TOKEN = %d\n", myrank, round, current_leader, recv_buf[1]);
 					fflush(stdout);
 
-
 					MPI_Send(recv_buf, 2, MPI_INT, succ, LEADER_ELECTION_RESULT_MSG_TAG, comm);
 				}
 			}
@@ -196,7 +213,6 @@ int main(int argc, char *argv[]) {
 		MPI_Barrier(comm);
 	}
 	printf("\n** Leader for NODE %d = %d\n", myrank, current_leader);
-
 
 	MPI_Finalize();
 	return 0;
